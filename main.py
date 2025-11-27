613326a875c446d104cb49c1d5bd71b2700855a4
MortenMDahl <mortenmdahl@outlook.com>
2025-11-27 22:03:19 +0100

    feat: Add function to load camera calibration data from files.

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 import cv2 as cv
 import numpy as np
 import settings
+import camera_calibration
+
+# TODO: Load calibration data
+def load_calibration_data():
+    ports = camera_calibration.list_camera_ports()
+    calibration_data = {}
+    for i in range(0, len(ports)):
+        calibration_data[i] = {
+            "camera_matrix": np.load(f"calibration_data/camera_matrix_{i}.npy"),
+            "dist_coeffs": np.load(f"calibration_data/dist_coeffs_{i}.npy"),
+            "rotation_vectors": np.load(f"calibration_data/rotation_vectors_{i}.npy"),
+            "translation_vectors": np.load(f"calibration_data/translation_vectors_{i}.npy")
+        }
+    return calibration_data
+
 
 def main():
     pass
